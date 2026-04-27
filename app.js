b75612019022f5029cfc9936f91a5bd1a33cd4f4
HanaeBOUKRAA <hanae.boukraa1@gmail.com>
2026-04-27 13:57:50 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,98 @@
+// default map layer
+var map = L.map('map').setView([35.873132, -5.523136], 14);
+
+//google map
+googleHybrid = L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}',{
+    maxZoom: 40,
+    subdomains:['mt0','mt1','mt2','mt3']
+});
+
+googleHybrid.addTo(map);
+
+    function runDirection(start, end) {
+        
+        // recreating new map layer after removal
+        var map = L.map('map').setView([35.873132, -5.523136], 14);
+
+       //google map
+       googleHybrid = L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}',{
+       maxZoom: 40,
+       subdomains:['mt0','mt1','mt2','mt3']
+    });
+
+    googleHybrid.addTo(map);
+        
+        var dir = MQ.routing.directions();
+
+        dir.route({
+            locations: [
+                start,
+                end
+            ]
+        });
+    
+
+        CustomRouteLayer = MQ.Routing.RouteLayer.extend({
+            createStartMarker: (location) => {
+                var custom_icon;
+                var marker;
+
+                custom_icon = L.icon({
+                    iconUrl: 'img/red.png',
+                    iconSize: [20, 29],
+                    iconAnchor: [10, 29],
+                    popupAnchor: [0, -29]
+                });
+
+                marker = L.marker(location.latLng, {icon: custom_icon}).addTo(map);
+
+                return marker;
+            },
+
+            createEndMarker: (location) => {
+                var custom_icon;
+                var marker;
+
+                custom_icon = L.icon({
+                    iconUrl: 'img/blue.png',
+                    iconSize: [20, 29],
+                    iconAnchor: [10, 29],
+                    popupAnchor: [0, -29]
+                });
+
+                marker = L.marker(location.latLng, {icon: custom_icon}).addTo(map);
+
+                return marker;
+            }
+        });
+        
+        map.addLayer(new CustomRouteLayer({
+            directions: dir,
+            fitBounds: true
+        })); 
+    }
+
+
+// function that runs when form submitted
+function submitForm(event) {
+    event.preventDefault();
+
+    // delete current map layer
+    map.remove();
+
+    // getting form data
+    start = document.getElementById("start").value;
+    end = document.getElementById("destination").value;
+
+    // run directions function
+    runDirection(start, end);
+
+    // reset form
+    document.getElementById("form").reset();
+}
+
+// asign the form to form variable
+const form = document.getElementById('form');
+
+// call the submitForm() function when submitting the form
+form.addEventListener('submit', submitForm);
